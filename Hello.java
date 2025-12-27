3cd141ba85dfa294898da5239db4e9a17ebea87e
sampathkumarsarveshwaran-cell <sampathkumarsarveshwaran@gmail.com>
2025-12-27 12:39:03 -0600

    Enhance Hello.java to include variable declarations and print statements for various data types

--- Hello.java
+++ Hello.java
@@ -2,6 +2,24 @@ class Hello
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World!!");
+        int a = 5;
+        byte b = 10;
+        short c = 15;
+        long d = 20L;
+        float e = 3.2f;
+        double f = 3.23445;
+        char g = 'A';
+        boolean h = true;
+        String str = "This is a string";
+        System.out.println("Integer: " + a);
+        System.out.println("Byte: " + b);
+        System.out.println("Short: " + c);
+        System.out.println("Long: " + d);   
+        System.out.println("Float: " + e);
+        System.out.println("Double: " + f);
+        System.out.println("Character: " + g);
+        System.out.println("Boolean: " + h);
+        System.out.println("String: " + str);
     }
 }

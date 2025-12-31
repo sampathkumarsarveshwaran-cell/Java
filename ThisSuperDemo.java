b995842da6b0458f9f0dcf8d2a72dc15b4a3b13d
sampathkumarsarveshwaran-cell <sampathkumarsarveshwaran@gmail.com>
2025-12-30 22:44:03 -0600

    Add ThisSuperDemo class to demonstrate constructor behavior and inheritance in Java

---
+++ ThisSuperDemo.java
@@ -0,0 +1,26 @@
+class A{
+    public A() // extends Object => This is how Java auto interprets this. 
+    {
+        super(); // Calls constructor or super class (A). Even if we dont specify, Java will internally call super()
+        System.out.println("A - Default Constructor");
+    }
+}
+class B extends A{
+    public B(){
+        super(); // Calls constructor or super class (A). Even if we dont specify, Java will internally call super()
+        System.out.println("B - Default Constructor");
+    }
+    public B(int n){
+        super(); // Calls constructor or super class (A). Even if we dont specify, Java will internally call super()
+        // In super if we pass n, then it will look for Constructor which takes one paramter and runs that instead of default constructor. 
+        // super(n) -> will call -> A(int n)
+        System.out.println("B - Parameterized Constructor");
+    }
+}
+public class ThisSuperDemo {
+    public static void main(String[] args) {
+        B b = new B();
+        System.out.println("\n");
+        B b1 = new B(5);
+    }
+}

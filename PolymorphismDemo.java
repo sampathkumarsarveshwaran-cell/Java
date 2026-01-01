5ed2be2cfaf308c82c27910d1c6a93b61131d5d6
sampathkumarsarveshwaran-cell <sampathkumarsarveshwaran@gmail.com>
2026-01-01 13:35:31 -0600

    Add PolymorphismDemo class with documentation on compile-time and run-time polymorphism

---
+++ PolymorphismDemo.java
@@ -0,0 +1,10 @@
+/*
+Polymorphism -> Many Behaviours
+Two types 
+1. Complile Time -> Method Overloading. Add(n1,n2) and Add(n1,n2,n3). During compile time which method will be identified.
+2. Run Time -> Overriding. Class A add(). Class B extends A add(). During run time which method will be identified.
+*/
+
+public class PolymorphismDemo {
+    
+}
